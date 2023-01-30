c00072196bb40fb48be3f82c12ae62df787fd57e
KhMansur <74125162+KhMansur@users.noreply.github.com>
2023-01-30 19:51:12 +0500

    Clientttttt

--- CarSale/Users/Clienttttttt.cs
+++ CarSale/Users/Clienttttttt.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CarSale.Users
 {
-    public class Client
+    public class Clientttttt
     {
         public int id { get; set; }
         public string name { get; set; }
